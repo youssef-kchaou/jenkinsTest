b9aac02c85d31092d4c4d0e6bc660fafcba7bb2c
youssek-kchaou <youssek.kchaou@esprit.tn>
2024-10-12 15:12:09 +0100

    first push

---
+++ src/main/java/com/esprit/microservice/testjenkinsstage/TestJenkinsStageApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.microservice.testjenkinsstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkinsStageApplication.class, args);
+    }
+
+}
